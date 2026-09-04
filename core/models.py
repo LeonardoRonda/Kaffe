9cabda8e45f7f21e24da8985a6aed121162e3288
yo-sud <yamil.ochoa@tecsup.edu.pe>
2026-09-03 22:26:03 -0500

    Implementado datos estáticos

--- core/models.py
+++ core/models.py
@@ -1,3 +1,38 @@
-from django.db import models
-
-# Create your models here.
+# Datos estáticos de productos (sin base de datos)
+PRODUCTOS = [
+    {
+        "nombre": "Café Americano",
+        "categoria": "bebida",
+        "precio": 2500,
+        "descripcion": "Café negro clásico",
+        "disponible": True
+    },
+    {
+        "nombre": "Latte",
+        "categoria": "bebida",
+        "precio": 3200,
+        "descripcion": "Café con leche vaporizada",
+        "disponible": True
+    },
+    {
+        "nombre": "Croissant",
+        "categoria": "postre",
+        "precio": 2800,
+        "descripcion": "Croissant de mantequilla",
+        "disponible": True
+    },
+    {
+        "nombre": "Brownie",
+        "categoria": "postre",
+        "precio": 3000,
+        "descripcion": "Brownie de chocolate",
+        "disponible": False
+    },
+    {
+        "nombre": "Leche de almendra",
+        "categoria": "extra",
+        "precio": 500,
+        "descripcion": "Alternativa vegetal",
+        "disponible": True
+    },
+]
